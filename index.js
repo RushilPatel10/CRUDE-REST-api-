3a71a2ed7dbbffd1f538035bd2ac345039a1bd93
Rushil patel <patelrushil1510@gmail.com>
2024-06-09 14:02:49 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const { json } = require("body-parser");
 
 const app = express();
 const port = 8000;
+
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.get("/users", (req, res) => {
@@ -21,40 +24,43 @@ app.get("/api/users", (req, res) => {
     return res.json(users);
 });
 
-app.route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        return res.json(user);
-    })
-    .patch((req, res) => {
-        // edit user
-        return res.json({ status: "pending" })
-    })
-    .delete((req, res) => {
-        // delete user
-        return res.json({ status: "pending" })
-    })
+// app.route('/api/users/:id')
+//     .get((req, res) => {
+//         const id = Number(req.params.id);
+//         const user = users.find((user) => user.id === id);
+//         return res.json(user);
+//     })
+//     .patch((req, res) => {
+//         // edit user
+//         return res.json({ status: "pending" })
+//     })
+//     .delete((req, res) => {
+//         // delete user
+//         return res.json({ status: "pending" })
+//     })
 
-// app.get("/api/users/:id", (req, res) => {
-//     const id = Number(req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     return res.json(user);
-// })
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
 
-app.post("/api/user", (req, res) => {
-    // create new user
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    users.push({ ...body, id: users.length });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status: "success", id: users.length });
+    })
+})
+
+app.patch("/api/users/:id", (req, res) => {
+    // edit the user
     return res.json({ status: "pending" });
 })
 
-// app.patch("/api/user/:id", (req, res) => {
-//     // edit the user
-//     return res.json({ status: "pending" });
-// })
-
-// app.delete("/api/user/:id", (req, res) => {
-//     // delete the user
-//     return res.json({ status: "pending" });
-// })
+app.delete("/api/users/:id", (req, res) => {
+    // delete the user
+    return res.json({ status: "pending" });
+})
 
 app.listen(port, () => console.log(`SERVER started at Port:${port}`));
